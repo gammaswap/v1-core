dcd24a9d6fce3fae9dbb7e0879acbe8c2b5c7125
Sammy Rodriguez <samalot32@gmail.com>
2022-09-12 23:19:38 -0400

    setup base test for GammaPoolERC20

---
+++ test/GammaPoolERC20.test.ts
@@ -0,0 +1,56 @@
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+
+describe("GammaPoolERC20", function () {
+    beforeEach(async function () {
+        // instantiate a GammaPool
+    })
+
+    function mint() {}
+
+    describe("base functions are working", function () {
+
+        describe("read functions", function () {
+
+            it("name is called correctly", function () {
+
+            })
+
+            it("symbol is called correctly", function () {
+                
+            })
+
+            it("decimals is called correctly", function () {
+                
+            })
+
+            it("totalSupply is called correctly", function () {
+                
+            })
+
+            it("balanceOf is called correctly", function () {
+                
+            })
+
+            it("allowance is called correctly", function () {
+                
+            })
+        })
+
+        describe("write functions", function () {
+            
+            it("approve is called correctly", function () {
+                
+            })
+            
+            it("LP tokens are transferred correctly", function () {
+                
+            })
+
+            it("transferFrom is called correctly", function () {
+                
+            })
+
+        })
+    })
+})
