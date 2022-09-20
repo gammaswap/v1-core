7d727fa5eb41b2b0f5944391c46540ceb77b0c97
Daniel Alcarraz <dam417@nyu.edu>
2022-09-19 21:45:06 -0400

    update init code hash

--- contracts/libraries/AddressCalculator.sol
+++ contracts/libraries/AddressCalculator.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 library AddressCalculator {
     // update this value if GammaPool gets updated
-    bytes32 internal constant GAMMA_POOL_INIT_CODE_HASH = 0x1a42d5fdf711eb76137d6e0223c376e9e74f24c2d7fe03e84642a124419efd2c;
+    bytes32 internal constant GAMMA_POOL_INIT_CODE_HASH = 0x574120bdb0e7c5324d7d3fee61bff78ea641ef0b86365c957aa6bfc1e5cbe404;
 
     function getGammaPoolKey(address cfmm, uint24 protocol) internal pure returns(bytes32) {
         return keccak256(abi.encode(cfmm, protocol));
