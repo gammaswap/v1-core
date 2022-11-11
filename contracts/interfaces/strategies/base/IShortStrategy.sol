9e9caf883e0cc6844193de5d5543eeb8ee192329
Daniel Alcarraz <dam417@nyu.edu>
2022-11-11 06:40:26 -0500

    changing require errors to custom errors

--- contracts/interfaces/strategies/base/IShortStrategy.sol
+++ contracts/interfaces/strategies/base/IShortStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.0;
+pragma solidity 0.8.4;
 
 interface IShortStrategy {
     function _depositNoPull(address to) external returns(uint256 shares);
--- contracts/libraries/AddressCalculator.sol
+++ contracts/libraries/AddressCalculator.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.0;
+pragma solidity 0.8.4;
 
 library AddressCalculator {
     // update this value if GammaPool gets updated
-    bytes32 internal constant GAMMA_POOL_INIT_CODE_HASH = 0xd11b91f1f3f28c55187c1be1b967960e417617b8aef172ac29b3eaf973cef859;
+    bytes32 internal constant GAMMA_POOL_INIT_CODE_HASH = 0xc701aa6ce9b9b1cc9d7d57bdffeb9935e635e2e37da96f50f37302d1e92c13b6;
 
     function getGammaPoolKey(address cfmm, uint24 protocol) internal pure returns(bytes32) {
         return keccak256(abi.encode(cfmm, protocol));
