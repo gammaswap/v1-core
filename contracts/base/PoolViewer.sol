cb17a972492292580734f18c567b5c6de6303471
danielalcarraz <dam417@nyu.edu>
2023-09-04 09:22:42 -0400

    downgrading to solidity version 0.8.19

--- contracts/base/PoolViewer.sol
+++ contracts/base/PoolViewer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 import "../libraries/GammaSwapLibrary.sol";
 import "../interfaces/IPoolViewer.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,11 +23,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.21",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 100,
       },
     },
   },
