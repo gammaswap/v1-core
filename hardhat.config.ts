95e45be6dfb03268125f5b071fee68f067acd8d4
Daniel Alcarraz <dam417@nyu.edu>
2024-01-18 16:12:31 -0500

    change update parameters in gammapool error codes to lower contract size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,6 +36,13 @@ const config: HardhatUserConfig = {
       },
       chainId: 421613,
     },
+    arbitrumSepolia: {
+      url: `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_ARBITRUM_SEPOLIA_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.ARBITRUM_SEPOLIA_MNEMONIC || "",
+      },
+      chainId: 421614,
+    },
     arbitrum: {
       url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ARBITRUM_API_KEY}`,
       accounts: {
@@ -53,6 +60,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://api-goerli.arbiscan.io/api",
           browserURL: "https://goerli.arbiscan.io",
+        },
+      },
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io",
         },
       },
     ],
